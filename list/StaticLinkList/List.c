01e6d06e60d0dc92d2828bccbac46104f5122578
TomCao <jiepengthegreat@126.com>
2015-07-13 07:29:46 +0800

    static linked list

--- list/StaticLinkList/List.c
+++ list/StaticLinkList/List.c
@@ -3,3 +3,19 @@ typedef struct{
 	ElemType data;
 	int cur;
 }Component, StaticLinkList[MAXSIZE];
+
+Status InitList(StaticLinkList space){
+	int i;
+	for (i = 0; i < MAXSIZE - 1; i++)
+		space[i].cur = i + 1;
+	space[MAXSIZE - 1].cur = 0;
+	return OK;
+}
+
+int Malloc_SLL(StaticLinkList space){
+	int i = space[0].cur;
+
+	if(space[0].cur)
+		space[0].cur = space[i].cur;
+	return i;
+}
